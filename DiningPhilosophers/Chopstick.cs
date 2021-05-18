ab226a37375017fa9a8a63d823ef2c17e0b69df2
Luis Lopez <darkdestruktion@gmail.com>
2021-05-18 17:00:36 -0500

    done

---
+++ DiningPhilosophers/Chopstick.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiningPhilosophers
+{
+    class Chopstick
+    {
+        private bool taken = false;
+
+        private int identity;
+
+        public Chopstick(int id)
+        {
+            identity = id;
+        }
+
+        public void Get()
+        {
+            lock (this)
+            {
+                while (taken)
+                {
+                    Monitor.Wait(this);
+                }
+                taken = true;
+
+                Console.WriteLine("Chopstick " + identity + " tomado");
+            }
+        }
+
+        public void Put()
+        {
+            lock(this)
+            {
+                taken = false;
+                Console.WriteLine("Chopstick " + identity + " bajado a la mesa.");
+                Monitor.Pulse(this);
+            }
+        }
+
+    }
+}
---
+++ DiningPhilosophers/Philosopher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiningPhilosophers
+{
+    class Philosopher
+    {
+        protected int identity;
+        protected Chopstick left;
+        protected Chopstick right;
+        private int timesEat;
+
+        public Philosopher(int id, Chopstick left, Chopstick right)
+        {
+            identity = id;
+            this.left = left;
+            this.right = right;
+        }
+
+        public void RunPhilosopher()
+        {
+            while (timesEat < 5)
+            {
+                Console.WriteLine("Filosofo " + identity + " esta pensando");
+
+                Console.WriteLine("Filosofo " + identity + " tiene hambre");
+
+                if (identity % 2 == 0)
+                {
+                    left.Get();
+                    Console.WriteLine("El filosofo " + identity + " tieme el chopstick izquierdo");
+                }
+                else
+                {
+                    right.Get();
+                    Console.WriteLine("El filosofo " + identity + " tomo el chopstick derecho");
+                }
+
+                if (identity % 2 == 0)
+                {
+                    right.Get();
+                    Console.WriteLine("El filosofo " + identity + " tomo el chostick derecho");
+                    Console.WriteLine("Ha comido " + ++timesEat + " veces");
+                }
+                else
+                {
+                    left.Get();
+                    Console.WriteLine("El filosofo " + identity + " tomo el chostick izquierdo");
+                    Console.WriteLine("Ha comido " + ++timesEat + " veces");
+                }
+
+                right.Put();
+                left.Put();
+            }
+        }
+
+
+    }
+}
+;
